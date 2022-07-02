fe2362a5e4f7bd41bf4e2d3aeff52b59c6e482ef
Fabricio Tonossu <fabriciotonossu94@gmail.com>
2022-07-01 20:43:18 -0400

    Melhorias JS

--- Projeto/js/main.js
+++ Projeto/js/main.js
@@ -2,8 +2,35 @@ console.log("ola mundo")
 const thumb = document.querySelectorAll('.imagem__thumb')
 const imagem = document.querySelector('.imagem')
 
-for(i=0; i < thumb.length; i++){
-    thumb[i].addEventListener('click', function(){
-        imagem.setAttribute('src', './images/image-product-2.jpg')
-    })
-}
+const listaImagens = ['./images/image-product-1', './images/image-product-2.jpg', './images/image-product-3.jpg', './images/image-product-4.jpg']
+
+console.log('Teste ${imagem} correto')
+
+
+thumb[0].addEventListener('click', function(){
+    imagem.setAttribute('src', `./images/image-product-1.jpg`)  
+})
+thumb[1].addEventListener('click', function(){
+    imagem.setAttribute('src', `./images/image-product-2.jpg`)  
+})
+thumb[2].addEventListener('click', function(){
+    imagem.setAttribute('src', `./images/image-product-3.jpg`)  
+})
+thumb[3].addEventListener('click', function(){
+    imagem.setAttribute('src', `./images/image-product-4.jpg`)  
+})
+
+const aumentar = document.querySelector('.quantidade-aumentar');
+const diminuir = document.querySelector('.quantidade-diminuir');
+const quantidade = document.querySelector('.quantidade-numero');
+
+aumentar.addEventListener('click', function(){
+    quantidade.textContent = parseInt(quantidade.textContent) + 1;
+})
+diminuir.addEventListener('click', function(){
+    if(quantidade.textContent > 0){
+        quantidade.textContent = parseInt(quantidade.textContent) - 1;
+    } else {
+        quantidade.textContent = 0;
+    }
+})
